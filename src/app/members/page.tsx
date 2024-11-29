ff43fa322bae2f8c23950829d8d1b461940ed361
fwiden <fredrik.widen@b3.se>
2024-11-29 15:28:33 +0100

    end of section 2

---
+++ src/app/members/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+
+export default function MembersPage() {
+  return (
+    <>
+      <div className="text-3xl">This is MembersPage</div>
+      <Link href="/">Go back home</Link>
+    </>
+  );
+}
